99c921da9d80cd4a55f93e5bdf70031b04decee5
znliunwpugmail <znliunwpu@gmail.com>
2018-03-09 14:00:47 +0800

    harris

---
+++ harris.h
@@ -0,0 +1,43 @@
+#ifndef HARRIS_H
+#define HARRIS_H
+
+#include<iostream>
+#include "cv.h"
+#include "highgui.h"
+using namespace cv;
+using namespace std;
+
+
+class Harris
+{
+    public:
+        Harris();
+        Harris(char*const image_name);
+        virtual ~Harris();
+    protected:
+    private:
+        IplImage *initImage;
+        IplImage *grayImage_8U;
+        IplImage *grayImage_64F;
+        IplImage *Ix,*Iy,*Ixx,*Iyy,*Ixy;
+        IplImage *A,*B,*C;
+
+
+        IplImage *harris_image;
+
+
+        double*  gauss_weight;
+
+
+        IplImage* Mul(IplImage* M1,IplImage*M2);
+
+
+        IplImage* MW(IplImage *M);
+        IplImage* harris_conner();
+        IplImage* nonMaxValueSuppression();
+
+
+};
+
+
+#endif // HARRIS_H
